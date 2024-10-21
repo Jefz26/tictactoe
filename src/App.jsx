6547e5f119155b14d8baa242eb7f00872bfd38bd
Jefz26 <107189810+Jefz26@users.noreply.github.com>
2024-10-21 22:08:02 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -71,9 +71,9 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = 'Go to move #' + move;
+      description = 'Move ' + move;
     } else {
-      description = 'Go to game start';
+      description = 'Restart';
     }
     return (
       <li key={move}>
